8e9898e405c4b298fcd75bfeed9264493c5426cf
fikret <61146337+FikretCansel@users.noreply.github.com>
2021-05-03 22:53:49 +0300

    Update firebase.js

--- Redux-Version/src/firebase.js
+++ Redux-Version/src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig =  {
-  apiKey: "AIzaSyA-1V0fWxS2k5ZkjW-y3rJ6cb2lCF5A5po",
-  authDomain: "amozonweb.firebaseapp.com",
-  databaseURL: "https://amozonweb.firebaseio.com",
-  projectId: "amozonweb",
-  storageBucket: "amozonweb.appspot.com",
-  messagingSenderId: "299332513609",
-  appId: "1:299332513609:web:83a7a93f5164273c237498",
-  measurementId: "G-3535Q0DJ9D"
+  yourKey
   };
 const firebaseApp=firebase.initializeApp(firebaseConfig);
 
